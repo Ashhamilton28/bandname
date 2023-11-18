899af24f0c4a08388f8fdaf6d9b2833fdc58ab6c
Ashley <Ashhamilton22888@gmail.com>
2023-11-18 10:40:16 -0500

    first commit

---
+++ bandname.py
@@ -0,0 +1,9 @@
+# Create a greeting for your program 
+
+# Ask user for city they grew up in 
+
+# Ask the user for the name of a pet 
+
+# combine the name of their city and pet and show their band name 
+
+# Make sure the input cursor shows a new line 
