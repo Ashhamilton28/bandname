5afd76bbe8ea00618004d61042c8b801a3d1bd37
Ashley <Ashhamilton22888@gmail.com>
2023-11-18 10:48:22 -0500

    changed {city} {pet_name} to {band_name} on line 12

--- bandname.py
+++ bandname.py
@@ -3,9 +3,11 @@ print("Welcome to Band Name Generator!")
 
 # Ask user for city they grew up in 
 city=input("What city did you grow up in? ").strip()
+
 # Ask the user for the name of a pet 
 pet_name=input("What is the name of your pet? ").strip()
+
 # combine the name of their city and pet and show their band name 
 band_name=(f"{city} {pet_name}")
-print(f"Your band name is: {city} {pet_name}")
+print(f"Your band name is: {band_name}")
  
