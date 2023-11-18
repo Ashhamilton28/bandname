700ce6c185f3ec873ca02e091f308f919e736632
Ashley <Ashhamilton22888@gmail.com>
2023-11-18 10:46:22 -0500

    added python code

--- bandname.py
+++ bandname.py
@@ -1,9 +1,11 @@
 # Create a greeting for your program 
+print("Welcome to Band Name Generator!")
 
 # Ask user for city they grew up in 
-
+city=input("What city did you grow up in? ").strip()
 # Ask the user for the name of a pet 
-
+pet_name=input("What is the name of your pet? ").strip()
 # combine the name of their city and pet and show their band name 
-
-# Make sure the input cursor shows a new line 
+band_name=(f"{city} {pet_name}")
+print(f"Your band name is: {city} {pet_name}")
+ 
